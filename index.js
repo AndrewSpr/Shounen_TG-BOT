615dcd17e436a0c960a138ade624bd632379c197
AndrewSpr <htfigshow@gmail.com>
2022-09-23 20:27:54 +0300

    redesigned function, hugs has become a universal command handler; added a module that contains commands and answer choices; now the bot responds to any case

--- index.js
+++ index.js
@@ -2,37 +2,35 @@ const TOKEN = "5625717603:AAEM-f_lKhTX_CbiU-gwTT_4Dhpxd5DFumQ";
 const TELEGRAM_API = require("node-telegram-bot-api");
 const { messageTypes } = require("node-telegram-bot-api/src/telegram");
 const { options } = require("nodemon/lib/config");
+const { commandList } = require("./commands");
 
 const BOT = new TELEGRAM_API(TOKEN, { polling: true });
 
 BOT.setMyCommands([{ command: "", description: "" }]);
 
-const hugs = () => {
+const commandHandler = () => {
   BOT.on("message", async (msg) => {
     const text = msg.text;
     const chatId = msg.chat.id;
 
     const sender = msg.from.username;
 
-    const phrasesArr = [
-      "крепко прижал(а) к себе",
-      "задушил(а), но не ляшками,",
-      "с любовью обнял(а)",
-      "до смерти заобнимал(а)",
-    ];
-
-    if (
-      text == "шоунен обнять" &&
-      msg.hasOwnProperty("reply_to_message") === true
-    ) {
-      await BOT.sendMessage(
-        chatId,
-        `@${sender} ${
-          phrasesArr[Math.floor(Math.random() * phrasesArr.length)]
-        } @${msg.reply_to_message.from.username}`
-      );
+    for (let i = 0; i < commandList.length; i++) {
+      if (
+        text.toLocaleLowerCase() == `шоунен ${commandList[i].commandName}` &&
+        msg.hasOwnProperty("reply_to_message") === true
+      ) {
+        await BOT.sendMessage(
+          chatId,
+          `@${sender} ${
+            commandList[i].phrases[
+              Math.floor(Math.random() * commandList[i].phrases.length)
+            ]
+          } @${msg.reply_to_message.from.username}`
+        );
+      }
     }
   });
 };
 
-hugs();
+commandHandler();
