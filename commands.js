144655b4cda5cd4da1d817c6946639caccea61f0
AndrewSpr <htfigshow@gmail.com>
2022-09-24 21:15:47 +0300

    bug with reading commands regardless of case was found - sent for rework; the command handler was redesigned and became a self-callable function; some new commands were added (kiss, confession); now the bot answers the command with a picture and a signature instead of a message; added dotenv functionality, the key was changed and sent to a hidden file

--- commands.js
+++ commands.js
@@ -8,6 +8,14 @@ module.exports = {
             "с любовью обнял(а)",
             "до смерти заобнимал(а)",
           ],
+          photo: [
+            "/img/hug1.webp",
+            "/img/hug2.webp",
+            "/img/hug3.webp",
+            "/img/hug4.webp",
+            "/img/hug5.webp",
+            "/img/hug6.webp",
+          ]
         }),
 
         (outrage = {
@@ -18,6 +26,51 @@ module.exports = {
             "нежно поимел(а)",
             "аккуратненько надругался(ась) над",
           ],
+          photo: [
+            "/img/outrage1.webp",
+            "/img/outrage2.webp",
+            "/img/outrage3.webp",
+            "/img/outrage4.webp",
+            "/img/outrage5.webp"
+          ]
+        }),
+
+        (confession = {
+          commandName: "признаться в любви",
+          phrases: [
+            "ради тебя готов(а) поставить онлайн игру на паузу,",
+            "рядом с тобой, буквально, затягивает,",
+            "тебя это... ну, знаешь............. слово на букву Л, вот...",
+            "предлагает стать старыми и несчастными вместе,",
+            "любит тебя больше, чем чипсеки с крабом,",
+            "заявляет, что будет любить тебя и с радостью терпеть всю жизнь,",
+          ],
+          photo: [
+            "/img/confession1.webp",
+            "/img/confession2.webp",
+            "/img/confession3.webp",
+            "/img/confession4.webp",
+            "/img/confession5.webp"
+          ]
+        }),
+
+        (kiss = {
+          commandName: "поцеловать",
+          phrases: [
+            "сладко поцеловал(а)",
+            "страстно прижал(а) к стенке и засосал(а)",
+            "от всей души проник(ла) своим языком в ротовую полость",
+            "с любовью чмокнул(а)",
+            "заломал(а) и зацеловал(а) до смерти",
+            "от всей души зачмокал(а)",
+          ],
+          photo: [
+            "/img/kiss1.webp",
+            "/img/kiss2.webp",
+            "/img/kiss3.webp",
+            "/img/kiss4.webp",
+            "/img/kiss5.webp"
+          ]
         }),
       ]
 }
--- index.js
+++ index.js
@@ -1,36 +1,47 @@
 const TOKEN = "5625717603:AAEM-f_lKhTX_CbiU-gwTT_4Dhpxd5DFumQ";
+require("dotenv").config();
 const TELEGRAM_API = require("node-telegram-bot-api");
 const { messageTypes } = require("node-telegram-bot-api/src/telegram");
+const fs = require("fs");
 const { options } = require("nodemon/lib/config");
 const { commandList } = require("./commands");
 
-const BOT = new TELEGRAM_API(TOKEN, { polling: true });
+const BOT = new TELEGRAM_API(process.env.TOKEN, { polling: true });
 
 BOT.setMyCommands([{ command: "", description: "" }]);
 
-const commandHandler = () => {
+const commandHandler = (() => {
+  // an arrow function that will process simple commands and send a response to them
   BOT.on("message", async (msg) => {
-    const text = msg.text;
-    const chatId = msg.chat.id;
+    const text = msg.text; // var stores data about text of the message that the user sent
+    const chatId = msg.chat.id; // var stores chat ID
 
-    const sender = msg.from.username;
+    const sender = msg.from.username; // var stores the data about the sender's ID
+
+    /* task of the loop is to look through all 
+    array indexes from the commands module
+    and find the properties of the command 
+    that corresponds to the one requested by the user */
 
     for (let i = 0; i < commandList.length; i++) {
       if (
-        text.toLocaleLowerCase() == `шоунен ${commandList[i].commandName}` &&
+        text == `шоунен ${commandList[i].commandName}` &&
         msg.hasOwnProperty("reply_to_message") === true
       ) {
-        await BOT.sendMessage(
+        const randomPhoto = Math.floor(
+          Math.random() * commandList[i].photo.length
+        ); // var stores the result of calculating the index of the photo that will be sent
+        const randomPhrase = Math.floor(
+          Math.random() * commandList[i].phrases.length
+        ); // var stores the result of calculating the index of the phrase that will be set as a description for the photo
+        await BOT.sendPhoto(
           chatId,
-          `@${sender} ${
-            commandList[i].phrases[
-              Math.floor(Math.random() * commandList[i].phrases.length)
-            ]
-          } @${msg.reply_to_message.from.username}`
+          fs.readFileSync(__dirname + commandList[i].photo[randomPhoto]),
+          {
+            caption: `@${sender} ${commandList[i].phrases[randomPhrase]} @${msg.reply_to_message.from.username}`,
+          }
         );
       }
     }
   });
-};
-
-commandHandler();
+})();
