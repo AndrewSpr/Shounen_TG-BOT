9e234d462ff886b2aee9b1946c462b567567c11a
AndrewSpr <htfigshow@gmail.com>
2022-10-03 18:06:18 +0300

    created several functions that are responsible for duels (fully implemented duel declaration function and acceptance of duels); started developing the duel handler

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { messageTypes } = require("node-telegram-bot-api/src/telegram");
 const fs = require("fs");
 const { options } = require("nodemon/lib/config");
 const { RolePlayCommandList } = require("./commands");
+const { resourceLimits } = require("worker_threads");
 
 const BOT = new TELEGRAM_API(process.env.TOKEN, { polling: true });
 
@@ -62,4 +63,115 @@ const commandHandler = (() => {
       }
     }
   });
+})();
+
+function createDuelistProfile(username, id, hp, weapon) {
+  this.username = username
+  this.id = id;
+  this.hp = hp;
+  this.weapon = weapon;
+}
+
+const hitDetector = () => {
+  const hit = Math.floor(Math.random()*2);
+  let hitResult = null;
+
+  if(hit == 1) {
+    hitResult = 'попал'
+  } else {
+    hitResult = 'промазал'
+  }
+  
+  return hitResult;
+}
+
+const duelsOptions = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        {
+          text: "Согласиться на дуэль",
+          callback_data: "Yes"
+        },
+        {
+          text: "Отказаться от дуэли",
+          callback_data: "No"
+        }
+      ]
+    ]
+  })
+}
+
+
+const DuelHandler = (chatId) => {
+
+  /*BOT.on("message", async (msg) => {
+    const text = msg.text || "";
+    const chatId = msg.chat.id;
+
+    const weaponsList = [
+      pistol = {
+        name: 'Pistol',
+        damage: 15
+      },
+      machineGun = {
+        name: 'Machinegun',
+        damage: 25
+      }
+    ]
+
+    if(text.toLowerCase() == 'шоунен дуэль' && msg.hasOwnProperty("reply_to_message") === true) {
+      const sender = new createDuelistProfile(msg.from.username, msg.from.id, 100, weaponsList[Math.floor(Math.random()*weaponsList.length)])
+      const recipient = new createDuelistProfile(msg.reply_to_message.from.username, msg.reply_to_message.from.id, 100, weaponsList[Math.floor(Math.random()*weaponsList.length)])
+      let senderHp = sender.hp, recipientHp = recipient.hp;
+      
+      for(let move = 0; senderHp > 0 || recipientHp > 0; move++) {
+        let senderHit = hitDetector(), recipientHit = hitDetector();
+        BOT.sendMessage(chatId, `@${sender.username} сделал выстрел из ${sender.weapon.name} и он ${senderHit} \n@${recipient.username} сделал выстрел из ${recipient.weapon.name} и он ${recipientHit}`);
+
+        if(senderHit == 'попал') {
+          recipientHp -= sender.weapon.damage
+        } else if (recipientHit == 'попал') {
+          senderHp -= recipient.weapon.damage
+        } else if (recipientHp <= 0) {
+          BOT.sendMessage(chatId, `@${recipient.username} проиграл в дуэли`)
+        } else if (senderHp <= 0) {
+          BOT.sendMessage(chatId, `@${sender.username} проиграл дуэль`)
+        }
+      }
+
+    }
+  })*/
+}
+
+const DuelingResponseHandler = (recipientId, recipientUsername) => {
+  BOT.on("callback_query", async (query) => {
+    const {chat, message_id} = query.message
+  
+    switch (true) {
+      case query.data === "Yes" && recipientId == query.from.id:
+        console.log(query)
+      break;
+      case query.data === "No" && recipientId == query.from.id:
+        BOT.editMessageText(`Дуэль отменена. @${recipientUsername} отказался от участия в этом кровопролитном деянии`, {
+          chat_id: chat.id,
+          message_id: message_id
+        })
+      break;
+    }
+  })
+}
+
+const DuelDeclaration = (() => {
+  BOT.on("message", async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text || "";
+
+    const sender = msg.from.username
+
+    if (text.toLowerCase() == 'шоунен дуэль' && msg.hasOwnProperty("reply_to_message") === true) {
+      await BOT.sendMessage(chatId, `@${sender} снял дедовскую перчатку со своей правой руки и швырнул прямо вам в лицо, объявив дуэль, @${msg.reply_to_message.from.username}\n\nЧтобы согласиться, или отказаться, нажмите соответствующую кнопку`, duelsOptions)
+      DuelingResponseHandler(msg.reply_to_message.from.id, msg.reply_to_message.from.username);
+    }
+  })
 })();
